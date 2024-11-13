62ac7bfc8dbebfc843c87905a1fc126c0f166126
Richard <richard@diffusionapps.com>
2024-11-13 13:22:42 +0200

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,8 +1,17 @@
 var xhr= new XMLHttpRequest();
-xhr.open('GET', 'main.html', true);
+xhr.open('GET', 'https://raw.githubusercontent.com/R1CH101/mtassets/refs/heads/main/main.html', true);
 xhr.onreadystatechange= function() {
     if (this.readyState!==4) return;
     if (this.status!==200) return; // or whatever error handling you want
-    document.documentElement.outerHTML= this.responseText;
+    document.querySelector("html").innerHTML= this.responseText;
 };
 xhr.send();
+var tag = document.createElement("script");
+tag.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.bundle.min.js";
+document.getElementsByTagName("head")[0].appendChild(tag);
+var tag = document.createElement("script");
+tag.src = "assets/js/functions.js";
+document.getElementsByTagName("head")[0].appendChild(tag);
+var tag = document.createElement("script");
+tag.src = "assets/js/saavn-search.js";
+document.getElementsByTagName("head")[0].appendChild(tag);
