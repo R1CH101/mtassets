a68c4e87c1108153724f45b77e9fc17b9e87be05
Richard <richard@diffusionapps.com>
2024-11-13 13:55:51 +0200

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,17 +1,22 @@
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'https://raw.githubusercontent.com/R1CH101/mtassets/refs/heads/main/main.html', true);
-xhr.onreadystatechange= function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    document.querySelector("html").innerHTML= this.responseText;
-};
-xhr.send();
-var tag = document.createElement("script");
+function fetchHtml() {
+    fetch('https://raw.githubusercontent.com/R1CH101/mtassets/refs/heads/main/main.html')
+    .then((response) => {
+      return response.text();
+    })
+    .then((html) => {
+        document.querySelector("html").innerHTML = html  
+        var tag = document.createElement("script");
+tag.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+document.body.appendChild(tag);   
+        var tag = document.createElement("script");
 tag.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.bundle.min.js";
-document.getElementsByTagName("head")[0].appendChild(tag);
+document.body.appendChild(tag);
 var tag = document.createElement("script");
 tag.src = "assets/js/functions.js";
-document.getElementsByTagName("head")[0].appendChild(tag);
+document.body.appendChild(tag);
 var tag = document.createElement("script");
 tag.src = "assets/js/saavn-search.js";
-document.getElementsByTagName("head")[0].appendChild(tag);
+document.body.appendChild(tag);
+    });
+  }
+  fetchHtml()
