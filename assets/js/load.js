4a93119483421a72df1ce2716587f489af4ff383
Richard <richard@diffusionapps.com>
2024-11-13 13:59:27 +0200

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -12,10 +12,10 @@ document.body.appendChild(tag);
 tag.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.bundle.min.js";
 document.body.appendChild(tag);
 var tag = document.createElement("script");
-tag.src = "assets/js/functions.js";
+tag.src = "https://raw.githubusercontent.com/R1CH101/mtassets/refs/heads/main/assets/js/functions.js";
 document.body.appendChild(tag);
 var tag = document.createElement("script");
-tag.src = "assets/js/saavn-search.js";
+tag.src = "https://raw.githubusercontent.com/R1CH101/mtassets/refs/heads/main/assets/js/saavn-search.js";
 document.body.appendChild(tag);
     });
   }
