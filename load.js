ce98af2c1b80c3818aa8a7cb5a55553c3aa7b61b
Richard <richard@diffusionapps.com>
2024-11-15 17:41:27 +0200

    Update load.js

--- load.js
+++ load.js
@@ -1,22 +1,28 @@
-function fetchHtml() {
-    fetch('https://mta.apps.lilleycloud.com/main.html')
+
+let cdn = "https://mtadev.apps.lilleycloud.com/";
+
+function loadApp() {
+    fetch(cdn + 'app.html')
     .then((response) => {
       return response.text();
     })
     .then((html) => {
         document.querySelector("html").innerHTML = html  
         var tag = document.createElement("script");
-tag.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-document.body.appendChild(tag);   
-        var tag = document.createElement("script");
-tag.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.bundle.min.js";
-document.body.appendChild(tag);
-var tag = document.createElement("script");
-tag.src = "https://mta.apps.lilleycloud.com/assets/js/functions.js";
-document.body.appendChild(tag);
-var tag = document.createElement("script");
-tag.src = "https://mta.apps.lilleycloud.com/assets/js/saavn-search.js";
-document.body.appendChild(tag);
+tag.src = "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries";
+document.head.appendChild(tag);   
     });
   }
-  fetchHtml()
+
+function loadPage(pageName) {
+    fetch(cdn + pageName + '.html')
+    .then((response) => {
+      return response.text();
+    })
+    .then((html) => {
+        document.querySelector("body").innerHTML = html  
+    });
+  }
+
+loadApp();
+loadPage("search");
